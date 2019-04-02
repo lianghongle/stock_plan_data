0a72d8ca0c8129b19b6fe89aff775d5f4149a4f4
神圣小树 <lianghogle@163.com>
2019-04-02 11:43:57 +0800

    add table_pro_stock_basic.sql，table_stock_basic.sql

---
+++ doc/sql/table_pro_stock_basic.sql
@@ -0,0 +1,21 @@
+-- 2019-04-02 11:42:27
+create table pro_stock_basic (
+    `index`      BIGINT(19)   null,
+    ts_code      VARCHAR(10)  null comment 'TS代码',
+    symbol       VARCHAR(6)   not null comment '股票代码'
+        primary key,
+    name         VARCHAR(15)  null comment '股票名称',
+    area         VARCHAR(10)  null comment '所在地域',
+    industry     VARCHAR(10)  null comment '所属行业',
+    fullname     VARCHAR(100) null comment '股票全称',
+    enname       VARCHAR(100) null comment '英文全称',
+    market       VARCHAR(10)  null comment '市场类型 （主板/中小板/创业板）',
+    exchange     VARCHAR(10)  null comment '交易所代码',
+    curr_type    VARCHAR(10)  null comment '交易货币',
+    list_status  VARCHAR(1)   null comment '上市状态： L上市 D退市 P暂停上市',
+    list_date    DATE(10) null comment '上市日期',
+    delist_date  DATE(10) null comment '退市日期',
+    is_hs        VARCHAR(1)   null comment '是否沪深港通标的，N否 H沪股通 S深股通',
+    created_date DATE(10) null
+);
+
---
+++ doc/sql/table_stock_basic.sql
@@ -0,0 +1,29 @@
+-- 2019-04-02 11:42:36
+create table stock_basic (
+    code             VARCHAR(6)    not null comment '代码'
+        primary key,
+    name             VARCHAR(15)   null comment '名称',
+    industry         VARCHAR(10)   null comment '所属行业',
+    area             VARCHAR(10)   null comment '地区',
+    pe               DOUBLE(22, 0) null comment '市盈率',
+    outstanding      DOUBLE(22, 0) null comment '流通股本(亿)',
+    totals           DOUBLE(22, 0) null comment '总股本(亿)',
+    totalAssets      DOUBLE(22, 0) null comment '总资产(万)',
+    liquidAssets     DOUBLE(22, 0) null comment '流动资产',
+    fixedAssets      DOUBLE(22, 0) null comment '固定资产',
+    reserved         DOUBLE(22, 0) null comment '公积金',
+    reservedPerShare DOUBLE(22, 0) null comment '每股公积金',
+    esp              DOUBLE(22, 0) null comment '每股收益',
+    bvps             DOUBLE(22, 0) null comment '每股净资',
+    pb               DOUBLE(22, 0) null comment '市净率',
+    timeToMarket     INT(10)       null comment '上市日期',
+    undp             DOUBLE(22, 0) null comment '未分利润',
+    perundp          DOUBLE(22, 0) null comment '每股未分配',
+    rev              DOUBLE(22, 0) null comment '收入同比(%)',
+    profit           DOUBLE(22, 0) null comment '利润同比(%)',
+    gpr              DOUBLE(22, 0) null comment '毛利率(%)',
+    npr              DOUBLE(22, 0) null comment '净利润率(%)',
+    holders          INT(10)       null comment '股东人数',
+    created_date     TEXT(65535)   null
+);
+
