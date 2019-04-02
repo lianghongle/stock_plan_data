d0ce7d7b9496eb82cc63dd7ce50c0e6286ffb89f
神圣小树 <lianghogle@163.com>
2019-04-02 12:44:56 +0800

    update

--- bin/pro/stock_basic.py
+++ bin/pro/stock_basic.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append('./../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 
 from datetime import datetime, timedelta
 import numpy as np
