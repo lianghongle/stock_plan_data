ef0e7e02014c6937e4b0d4286ae3e43b99ccdeb7
神圣小树 <lianghogle@163.com>
2019-04-11 08:38:45 +0800

    add table_area.sql，table_industry.sql

---
+++ doc/sql/table_area.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+create table stock_area (
+    id           INT(10) auto_increment
+        primary key,
+    area         VARCHAR(10) null,
+    created_date DATE null,
+    constraint stock_area_name_uindex
+        unique (area)
+);
+
---
+++ doc/sql/table_industry.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+create table stock_industry (
+    id           INT(10) auto_increment
+        primary key,
+    industry     VARCHAR(10) null,
+    created_date DATE null,
+    constraint stock_industry_name_uindex
+        unique (industry)
+);
+
